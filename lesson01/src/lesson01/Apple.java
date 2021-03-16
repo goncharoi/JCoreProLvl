92bedf7c0f727eace3601bca66959906ddccb257
goncharoi <s--e--t@yandex.ru>
2021-03-17 01:13:33 +0500

    clear

--- lesson01/src/lesson01/Apple.java
+++
@@ -1,7 +0,0 @@
-package lesson01;
-
-public class Apple extends Fruit{
-    public Apple() {
-        setMvWeight(1.0f);
-    }
-}
--- lesson01/src/lesson01/Orange.java
+++
@@ -1,7 +0,0 @@
-package lesson01;
-
-public class Orange extends Fruit{
-    public Orange() {
-        setMvWeight(1.5f);
-    }
-}
