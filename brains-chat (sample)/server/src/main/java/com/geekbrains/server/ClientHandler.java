5e60bea570205847491bba3aaaba8860047d6e9a
goncharoi <s--e--t@yandex.ru>
2021-03-21 18:19:05 +0500

    lesson02 done

---
+++ brains-chat (sample)/server/src/main/java/com/geekbrains/server/ClientHandler.java
@@ -0,0 +1,108 @@
+package com.geekbrains.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private String nickname;
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String msg = in.readUTF();
+                        // /auth login1 pass1
+                        if (msg.startsWith("/auth ")) {
+                            System.out.println("Попытка авторизации: "+msg);
+                            String[] tokens = msg.split("\\s");
+                            String nick = server.getAuthService().getNicknameByLoginAndPassword(tokens[1], tokens[2]);
+                            if (nick != null && !server.isNickBusy(nick)) {
+                                sendMsg("/authok " + nick);
+                                nickname = nick;
+                                server.subscribe(this);
+                                break;
+                            }
+                        }
+                    }
+                    while (true) {
+                        String msg = in.readUTF();
+                        if(msg.startsWith("/")) {
+                            if (msg.equals("/end")) {
+                                sendMsg("/end");
+                                break;
+                            }
+                            if(msg.startsWith("/w ")) {
+                                String[] tokens = msg.split("\\s", 3);
+                                server.privateMsg(this, tokens[1], tokens[2]);
+                            }
+//>>> Кусок про смену ника
+                            if(msg.startsWith("/chn")){
+                                String[] tokens = msg.split("\\s", 4); //nick login pass
+                                if(tokens[1] != null && !server.isNickBusy(tokens[1])){
+                                    if(server.getAuthService().changeNicknameByLoginAndPassword(tokens[1], tokens[2], tokens[3])) {
+                                        System.out.println("отправляю клиенту сообщение о смене ника");
+                                        sendMsg("/authok " + tokens[1]);
+                                        nickname = tokens[1];
+                                        System.out.println("Рассылаю новый список ников");
+                                        server.broadcastClientsList();
+                                    }
+                                }
+                            }
+//<<< Кусок про смену ника
+                        } else {
+                            server.broadcastMsg(nickname + ": " + msg);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    ClientHandler.this.disconnect();
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        server.unsubscribe(this);
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
