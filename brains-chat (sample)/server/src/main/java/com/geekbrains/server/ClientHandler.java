cdc16425d70fe42caf18264240f39ee00405fdac
goncharoi <s--e--t@yandex.ru>
2021-03-25 09:37:46 +0500

    lesson03 done

--- brains-chat (sample)/server/src/main/java/com/geekbrains/server/ClientHandler.java
+++ brains-chat (sample)/server/src/main/java/com/geekbrains/server/ClientHandler.java
@@ -24,50 +24,8 @@ public class ClientHandler {
             this.out = new DataOutputStream(socket.getOutputStream());
             new Thread(() -> {
                 try {
-                    while (true) {
-                        String msg = in.readUTF();
-                        // /auth login1 pass1
-                        if (msg.startsWith("/auth ")) {
-                            System.out.println("Попытка авторизации: "+msg);
-                            String[] tokens = msg.split("\\s");
-                            String nick = server.getAuthService().getNicknameByLoginAndPassword(tokens[1], tokens[2]);
-                            if (nick != null && !server.isNickBusy(nick)) {
-                                sendMsg("/authok " + nick);
-                                nickname = nick;
-                                server.subscribe(this);
-                                break;
-                            }
-                        }
-                    }
-                    while (true) {
-                        String msg = in.readUTF();
-                        if(msg.startsWith("/")) {
-                            if (msg.equals("/end")) {
-                                sendMsg("/end");
-                                break;
-                            }
-                            if(msg.startsWith("/w ")) {
-                                String[] tokens = msg.split("\\s", 3);
-                                server.privateMsg(this, tokens[1], tokens[2]);
-                            }
-//>>> Кусок про смену ника
-                            if(msg.startsWith("/chn")){
-                                String[] tokens = msg.split("\\s", 4); //nick login pass
-                                if(tokens[1] != null && !server.isNickBusy(tokens[1])){
-                                    if(server.getAuthService().changeNicknameByLoginAndPassword(tokens[1], tokens[2], tokens[3])) {
-                                        System.out.println("отправляю клиенту сообщение о смене ника");
-                                        sendMsg("/authok " + tokens[1]);
-                                        nickname = tokens[1];
-                                        System.out.println("Рассылаю новый список ников");
-                                        server.broadcastClientsList();
-                                    }
-                                }
-                            }
-//<<< Кусок про смену ника
-                        } else {
-                            server.broadcastMsg(nickname + ": " + msg);
-                        }
-                    }
+                    getAuth();
+                    receiveMsg();
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
@@ -79,6 +37,56 @@ public class ClientHandler {
         }
     }
 
+    public void receiveMsg() throws IOException{
+        while (true) {
+            String msg = in.readUTF();
+            if(msg.startsWith("/")) {
+                if (msg.equals("/end")) {
+                    sendMsg("/end");
+                    break;
+                }
+                if(msg.startsWith("/w ")) {
+                    String[] tokens = msg.split("\\s", 3);
+                    server.privateMsg(this, tokens[1], tokens[2]);
+                }
+//>>> Кусок про смену ника
+                if(msg.startsWith("/chn")){
+                    String[] tokens = msg.split("\\s", 4); //nick login pass
+                    if(tokens[1] != null && !server.isNickBusy(tokens[1])){
+                        if(server.getAuthService().changeNicknameByLoginAndPassword(tokens[1], tokens[2], tokens[3])) {
+                            System.out.println("отправляю клиенту сообщение о смене ника");
+                            sendMsg("/authok " + tokens[1]);
+                            nickname = tokens[1];
+                            System.out.println("Рассылаю новый список ников");
+                            server.broadcastClientsList();
+                        }
+                    }
+                }
+//<<< Кусок про смену ника
+            } else {
+                server.broadcastMsg(nickname + ": " + msg);
+            }
+        }
+    }
+
+    public void getAuth() throws IOException{
+        while (true) {
+            String msg = in.readUTF();
+            // /auth login1 pass1
+            if (msg.startsWith("/auth ")) {
+                System.out.println("Попытка авторизации: "+msg);
+                String[] tokens = msg.split("\\s");
+                String nick = server.getAuthService().getNicknameByLoginAndPassword(tokens[1], tokens[2]);
+                if (nick != null && !server.isNickBusy(nick)) {
+                    sendMsg("/authok " + nick);
+                    nickname = nick;
+                    server.subscribe(this);
+                    break;
+                }
+            }
+        }        
+    }
+    
     public void sendMsg(String msg) {
         try {
             out.writeUTF(msg);
